a9e724f4ed5986acd3fb9135564f4e1b100cf84b
Jiří Pudil <me@jiripudil.cz>
2025-12-01 22:16:44 +0100

    solve 01.2

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -18,7 +18,39 @@ pub fn part_one(input: &str) -> Option<u64> {
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+    let mut dial: i64 = 50;
+    let mut count = 0;
+
+    input.lines().for_each(|line| {
+        let previous = dial;
+        let direction = &line[0..1];
+        let distance = line[1..].parse::<i64>().unwrap();
+        dial += distance * match direction { "L" => -1, "R" => 1, _ => unreachable!() };
+
+        if previous == 0 && dial < 0 && dial % 100 != 0 {
+            count -= 1;
+        }
+
+        if dial == 0 {
+            count += 1;
+        }
+
+        while dial < 0 {
+            dial += 100;
+            count += 1;
+
+            if dial == 0 {
+                count += 1;
+            }
+        }
+
+        while dial >= 100 {
+            dial -= 100;
+            count += 1;
+        }
+    });
+
+    Some(count)
 }
 
 #[cfg(test)]
@@ -34,6 +66,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(6));
     }
 }
