28b3b84e161abcb088045efba61c6c1403139ed7
Jiří Pudil <me@jiripudil.cz>
2025-12-02 11:22:51 +0100

    solve 02.2

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -1,6 +1,6 @@
 advent_of_code::solution!(2);
 
-fn is_invalid(n: u64) -> bool {
+fn is_invalid_part_one(n: u64) -> bool {
     let n_str = n.to_string();
     if n_str.len() % 2 != 0 {
         return false;
@@ -37,7 +37,7 @@ pub fn part_one(input: &str) -> Option<u64> {
         let to = pair.next().unwrap().parse::<u64>().unwrap();
 
         for n in from..=to {
-            if is_invalid(n) {
+            if is_invalid_part_one(n) {
                 sum += n;
             }
         }
@@ -46,8 +46,51 @@ pub fn part_one(input: &str) -> Option<u64> {
     Some(sum)
 }
 
+fn is_invalid_part_two(n: u64) -> bool {
+    let n_str = n.to_string();
+    let threshold = n_str.len() / 2;
+    for offset in 1..=threshold {
+        if n_str.len() % offset != 0 {
+            continue
+        }
+
+        let prefix = &n_str[0..offset];
+
+        let mut cursor = offset;
+        let mut found = true;
+        while cursor < n_str.len() {
+            if &n_str[cursor..cursor + offset] != prefix {
+                found = false;
+                break;
+            }
+
+            cursor += offset;
+        }
+
+        if found {
+            return true
+        }
+    }
+
+    false
+}
+
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+    let mut sum = 0u64;
+    let ranges = input.split(",");
+    ranges.for_each(|range| {
+        let mut pair = range.splitn(2, "-");
+        let from = pair.next().unwrap().parse::<u64>().unwrap();
+        let to = pair.next().unwrap().parse::<u64>().unwrap();
+
+        for n in from..=to {
+            if is_invalid_part_two(n) {
+                sum += n;
+            }
+        }
+    });
+
+    Some(sum)
 }
 
 #[cfg(test)]
@@ -63,6 +106,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(4174379265));
     }
 }
