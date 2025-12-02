a3cf7fab00bb75aae664d40a2bb081e369e6b0fd
Jiří Pudil <me@jiripudil.cz>
2025-12-02 11:34:31 +0100

    merge 02 helper fn

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -1,55 +1,8 @@
 advent_of_code::solution!(2);
 
-fn is_invalid_part_one(n: u64) -> bool {
+fn is_invalid(n: u64, get_offsets: fn(u64) -> Vec<usize>) -> bool {
     let n_str = n.to_string();
-    if n_str.len() % 2 != 0 {
-        return false;
-    }
-
-    let threshold = n_str.len() / 2;
-    let offset = threshold;
-    let prefix = &n_str[0..offset];
-
-    let mut cursor = offset;
-    let mut found = true;
-    while cursor < n_str.len() {
-        if &n_str[cursor..cursor + offset] != prefix {
-            found = false;
-            break;
-        }
-
-        cursor += offset;
-    }
-
-    if found {
-        return true
-    }
-
-    false
-}
-
-pub fn part_one(input: &str) -> Option<u64> {
-    let mut sum = 0u64;
-    let ranges = input.split(",");
-    ranges.for_each(|range| {
-        let mut pair = range.splitn(2, "-");
-        let from = pair.next().unwrap().parse::<u64>().unwrap();
-        let to = pair.next().unwrap().parse::<u64>().unwrap();
-
-        for n in from..=to {
-            if is_invalid_part_one(n) {
-                sum += n;
-            }
-        }
-    });
-
-    Some(sum)
-}
-
-fn is_invalid_part_two(n: u64) -> bool {
-    let n_str = n.to_string();
-    let threshold = n_str.len() / 2;
-    for offset in 1..=threshold {
+    for offset in get_offsets(n) {
         if n_str.len() % offset != 0 {
             continue
         }
@@ -75,6 +28,32 @@ fn is_invalid_part_two(n: u64) -> bool {
     false
 }
 
+pub fn part_one(input: &str) -> Option<u64> {
+    let mut sum = 0u64;
+    let ranges = input.split(",");
+    ranges.for_each(|range| {
+        let mut pair = range.splitn(2, "-");
+        let from = pair.next().unwrap().parse::<u64>().unwrap();
+        let to = pair.next().unwrap().parse::<u64>().unwrap();
+
+        for n in from..=to {
+            let get_offset = |n: u64| -> Vec<usize> {
+                if n.to_string().len() % 2 != 0 {
+                    vec![]
+                } else {
+                    vec![n.to_string().len() / 2]
+                }
+            };
+
+            if is_invalid(n, get_offset) {
+                sum += n;
+            }
+        }
+    });
+
+    Some(sum)
+}
+
 pub fn part_two(input: &str) -> Option<u64> {
     let mut sum = 0u64;
     let ranges = input.split(",");
@@ -84,7 +63,12 @@ pub fn part_two(input: &str) -> Option<u64> {
         let to = pair.next().unwrap().parse::<u64>().unwrap();
 
         for n in from..=to {
-            if is_invalid_part_two(n) {
+            let get_offset = |n: u64| -> Vec<usize> {
+                let threshold = n.to_string().len() / 2;
+                (1..=threshold).collect()
+            };
+
+            if is_invalid(n, get_offset) {
                 sum += n;
             }
         }
