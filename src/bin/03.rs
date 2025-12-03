4cc89132c9fd70b2e76591a957c76fa83b681339
Jiří Pudil <me@jiripudil.cz>
2025-12-03 14:11:48 +0100

    solve 03.1

---
+++ src/bin/03.rs
@@ -0,0 +1,54 @@
+advent_of_code::solution!(3);
+
+pub fn part_one(input: &str) -> Option<u64> {
+    let lines = input.split("\n");
+    let mut sum = 0u64;
+
+    lines.for_each(|line| {
+        let mut largest_value_10 = 0u64;
+        let mut largest_index = 0usize;
+        let digits = line.chars().map(|c| c.to_digit(10).unwrap() as u64);
+        for (index, digit) in digits.clone().enumerate() {
+            if index == line.chars().count() - 1 {
+                break
+            }
+
+            if digit > largest_value_10 {
+                largest_value_10 = digit;
+                largest_index = index;
+            }
+        }
+
+        let mut largest_value_1 = 0;
+        for digit in digits.skip(largest_index + 1) {
+            if digit > largest_value_1 {
+                largest_value_1 = digit;
+            }
+        }
+
+        sum += largest_value_10 * 10 + largest_value_1;
+    });
+
+    Some(sum)
+}
+
+pub fn part_two(input: &str) -> Option<u64> {
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(357));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+}
