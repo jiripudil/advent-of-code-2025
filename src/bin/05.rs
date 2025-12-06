3f622d511d8f6f431054c8fc079308c036696dee
Jiří Pudil <me@jiripudil.cz>
2025-12-06 12:09:25 +0100

    solve 05.2

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -1,3 +1,5 @@
+use std::cmp;
+
 advent_of_code::solution!(5);
 
 pub fn part_one(input: &str) -> Option<u64> {
@@ -33,7 +35,37 @@ pub fn part_one(input: &str) -> Option<u64> {
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+    let mut fresh_ingredients: Vec<(u64, u64)> = vec![];
+
+    for line in input.split("\n") {
+        if line.is_empty() {
+            break
+        }
+
+        let mut range = line.split("-");
+        let start = range.next().unwrap().parse::<u64>().unwrap();
+        let end_inclusive = range.next().unwrap().parse::<u64>().unwrap();
+        fresh_ingredients.push((start, end_inclusive));
+    }
+
+    fresh_ingredients.sort_by(|(a, _), (b, _)| a.cmp(b));
+
+    let mut result: Vec<(u64, u64)> = vec![];
+    result.push(fresh_ingredients[0]);
+
+    for i in 1..fresh_ingredients.len() {
+        let current = fresh_ingredients[i];
+        let j = result.len() - 1;
+
+        if current.0 >= result[j].0 && current.0 <= result[j].1 {
+            result[j].1 = cmp::max(current.1, result[j].1);
+        } else {
+            result.push(current);
+        }
+    }
+
+    let no_of_fresh_ingredients = result.iter().fold(0, |acc, (start, end)| acc + (end - start + 1));
+    Some(no_of_fresh_ingredients)
 }
 
 #[cfg(test)]
@@ -49,6 +81,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(14));
     }
 }
